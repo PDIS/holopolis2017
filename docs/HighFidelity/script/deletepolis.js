80ed9ebe48f52871a04d03beae634406e4b60c03
XanxusX <tako30215@hotmail.com>
2017-11-08 14:59:37 +0800

    output log

--- docs/HighFidelity/script/deletepolis.js
+++ docs/HighFidelity/script/deletepolis.js
@@ -3,6 +3,8 @@ function deletepolis() {
   for (var i = 0; i < ids.length; i++) {
     var id = ids[i];
     var properties = Entities.getEntityProperties(id);
+    console.log(properties.name)
+    console.log(JSON.stringify(properties))
     if (properties.name == "polis-uncrewed-vehicle") {
       Entities.deleteEntity(id);
     }
