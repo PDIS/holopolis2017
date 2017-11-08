aa8a621fc71124a190270fdef6d3e973dc4e96cd
XanxusX <tako30215@hotmail.com>
2017-11-08 14:57:16 +0800

    add delete script

---
+++ docs/HighFidelity/script/deletepolis.js
@@ -0,0 +1,10 @@
+function deletepolis() {
+  var ids = Entities.findEntities(MyAvatar.position, 100);
+  for (var i = 0; i < ids.length; i++) {
+    var id = ids[i];
+    var properties = Entities.getEntityProperties(id);
+    if (properties.name == "polis-uncrewed-vehicle") {
+      Entities.deleteEntity(id);
+    }
+  }
+}
