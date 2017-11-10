5fb11316c82db8faf13c1a9534250462494e9a46
XanxusX <tako30215@hotmail.com>
2017-11-10 09:49:31 +0800

    test

---
+++ docs/HighFidelity/script/test.js
@@ -0,0 +1,7 @@
+var startTime = new Date();
+var func = function () {
+  console.log('start: ' + (new Date() - startTime));
+  for (var i = 0; i < 1000000000; i++) { }
+  console.log('end: ' + (new Date() - startTime));
+};
+setInterval(func, 100);
