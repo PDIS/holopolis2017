9cb0a79e2a6817ae2193cafe38d4ff6da178d4d2
XanxusX <tako30215@hotmail.com>
2017-11-10 10:21:03 +0800

    Update test.js

--- docs/HighFidelity/script/test.js
+++ docs/HighFidelity/script/test.js
@@ -1,7 +1,3 @@
-var startTime = new Date();
-var func = function () {
-  console.log('start: ' + (new Date() - startTime));
-  for (var i = 0; i < 1000000000; i++) { }
-  console.log('end: ' + (new Date() - startTime));
-};
-Script.setInterval(func, 100);
+var multiple_timer = Script.setInterval(function() {
+    print("Repeating timer fired!"); 
+    }, 1000);
