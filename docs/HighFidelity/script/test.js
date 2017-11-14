8060046b06a7a64745bc7c66023c8bb49dd68f3c
XanxusX <tako30215@hotmail.com>
2017-11-14 17:41:15 +0800

    test

--- docs/HighFidelity/script/test.js
+++ docs/HighFidelity/script/test.js
@@ -1,5 +1,7 @@
-var multiple_timer = Script.setInterval(function() {
-    var a = ['1','2','3']
-    var b = a.contains('2')
-    console.log(b)
-    }, 1000);
+var multiple_timer = Script.setInterval(function () {
+  var array = ['1', '2', '3', '1', '3', '4']
+  var result = array.filter(function (element, index, arr) {
+    return arr.indexOf(element) === index;
+  });
+  console.log(result);
+}, 1000);
