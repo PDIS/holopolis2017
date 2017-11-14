ee7fbdb4102a6329f6746558b71873107b99f178
XanxusX <tako30215@hotmail.com>
2017-11-14 17:48:21 +0800

    test

--- docs/HighFidelity/script/test.js
+++ docs/HighFidelity/script/test.js
@@ -1,5 +1,14 @@
 var multiple_timer = Script.setInterval(function () {
-  var array = ['1', '2', '3', '1', '3', '4']
+  var array = ['1', '2', '3']
+  var array2 = ['1', '3', '4']
+  for (var i in array) {
+    for (var j in array2) {
+      if (array[i] == array2[j]) {
+        array.splice(i,1)
+      }
+    }
+  }
+  console.log(array)
   var result = array.filter(function (element, index, arr) {
     return arr.indexOf(element) === index;
   });
