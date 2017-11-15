26c5345e3991dd5572553311f914c37bec74ae71
XanxusX <tako30215@hotmail.com>
2017-11-15 10:42:52 +0800

    test

--- docs/HighFidelity/script/test.js
+++ docs/HighFidelity/script/test.js
@@ -9,7 +9,7 @@ var multiple_timer = Script.setInterval(function () {
     }
   }
   console.log(array)
-  var obj = [{'id':1},{'id':3}]
+  var obj = [{'id':'1'},{'id':'3'}]
   for (var o of obj) {
     if (o.id.indexOf('3') >= 0) {
       console.log('ya')
